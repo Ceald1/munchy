b22b2c99b4fc1ba065b8b4655e80f63682eb27d3
ceald1 <ceald@ceald.cc>
2025-08-19 01:10:47 -0700

    multiple args

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ mod utils;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)] // Reads version from Cargo.toml, uses doc comment for about
 struct CLI {
-    op: String,
+    #[arg(num_args(0..))]
+    op: Vec<String>,
     #[arg(short, long, action = clap::ArgAction::SetTrue)] // Defines -v/--verbose flag
    verbose: bool,
 }
@@ -21,15 +22,17 @@ _____________,-.___     _
 fn main() {
     let cli = CLI::parse();
     println!("{}", ART);
-    let op:String = cli.op;
-    match op.as_str(){
-        "test" => {
-            let sysPID: u32 = utils::token::getSysProcess();
-            let woot: Option<HANDLE> = utils::token::get_system(sysPID);
-            println!("{:?}", woot)
-        }
-        _ => {
-            todo!("")
+    let op:Vec<String> = cli.op;
+    for o in op{
+        match o.as_str(){
+            "test" => {
+                let sysPID: u32 = utils::token::getSysProcess();
+                let woot: Option<HANDLE> = utils::token::get_system(sysPID);
+                println!("{:?}", woot)
+            }
+            _ => {
+                todo!("")
+            }
         }
     }
 }
